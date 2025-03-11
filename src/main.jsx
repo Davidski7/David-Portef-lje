3a1f4e67f0b026d3b9be71f5cb96eec9f67f6a20
Davidski7 <U2486889@rts-365.dk>
2025-03-11 14:04:00 +0100

    Hvad så

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/App.jsx";
-import Projects from "./pages/projects.jsx";
-import About from "./pages/about.jsx";
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
+
       </Routes>
     </BrowserRouter>
   </StrictMode>
